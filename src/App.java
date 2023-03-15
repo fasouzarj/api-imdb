49951296dd8e4df0b133163859a7eb3ddcf6b38f
Flavio Alves <flavio.alves84@gmail.com>
2023-03-15 19:33:16 -0300

    feat: update to use "getenv" for import apiKey

--- src/App.java
+++ src/App.java
@@ -1,4 +1,3 @@
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
@@ -6,15 +5,12 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.util.Properties;
 
 public class App {
     public static void main(String[] args) throws FileNotFoundException, IOException {
 
-        Properties props = new Properties();
-        props.load(new FileInputStream("config.properties"));
-      
-        String apiKey = props.getProperty("api.key");    
+        String apiKey = System.getenv("apiKey");
+
         String url = "https://imdb-api.com/en/API/Top250Movies/"+ apiKey;
         
         HttpClient client = HttpClient.newHttpClient();
